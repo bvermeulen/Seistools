0824fabadb17a09c4c25ffa0382f5a7c5027c294
bvermeulen <bruno_vermeulen2001@yahoo.com>
2020-01-04 20:25:59 +0400

    vib_attributes

--- vib_attributes.py
+++ vib_attributes.py
@@ -1,44 +1,40 @@
 # display vibe statistics
-from update_vaps import VpDb, VapsTable
-import pandas as pd
+import datetime
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+from update_vaps import VpDb
 
 fleets = 5
 
 class VpAttributes:
 
-    vib_attributes = {
-        'index': [],
-        'vibrator': [],
-        'avg_phase': [],
-        'peak_phase': [],
-        'avg_dist': [],
-        'peak_dist': [],
-        'avg_force': [],
-        'peak_force': [],
-    }
-
     @classmethod
     def select_data(cls, production_date):
         vp_db = VpDb()
-        cls.vaps_records = vp_db.retrieve_vaps_data_by_date(production_date)
-
-        for index, record in enumerate(cls.vaps_records):
-            cls.vib_attributes['index'].append(index)
-            cls.vib_attributes['vibrator'].append(record.vibrator)
-            cls.vib_attributes['avg_phase'].append(record.avg_phase)
-            cls.vib_attributes['peak_phase'].append(record.peak_phase)
-            cls.vib_attributes['avg_dist'].append(record.avg_dist)
-            cls.vib_attributes['peak_dist'].append(record.peak_dist)
-            cls.vib_attributes['avg_force'].append(record.avg_force)
-            cls.vib_attributes['peak_force'].append(record.peak_force)
+        cls.vaps_records_df = vp_db.get_vaps_data_by_date(production_date)
 
     @classmethod
     def plot_vaps_data(cls):
         fig1, ((ax0, ax1), (ax2, ax3), (ax4, ax5),
             ) = plt.subplots(nrows=3, ncols=2, figsize=(8, 8))
         plt.subplots_adjust(hspace=10)
-        ax0, ax1 = cls.plot_attribute(ax0, ax1, [
-            'peak_phase', 'Peak phase', 0, 20])
+
+        # plot peak distortion
+        settings = {
+            'key':'peak_dist',
+            'title': 'Peak Distorion',
+            'y-axis_title': 'Percentage',
+            'y-axis_min': 0,
+            'y-axis_max': 60,
+            'y-axis_int': 1,
+        }
+        ax0 = cls.plot_attribute(ax0, settings)
+
+        settings['title'] = 'Peak Distortion Density'
+        settings['y-axis_title'] = ''
+        ax1 = cls.plot_density(ax1, settings)
+
         # ax2, ax3 = cls.plot_peak_dist(ax2, ax3)
         # ax4, ax5 = cls.plot_peak_phase(ax4, ax5)
 
@@ -49,18 +45,74 @@ class VpAttributes:
         # ax8, ax9 = cls.plot_peak_dist(ax8, ax9)
         # ax10, ax11 = cls.plot_peak_phase(ax10, ax11)
 
+        fig1.tight_layout()
+        plt.show()
 
     @classmethod
-    def plot_attribute(cls, axis1, axis2, config):
+    def plot_attribute(cls, axis, settings):
+        key = settings['key']
+        axis.set_title(settings['title'])
+        axis.set_ylabel(settings['y-axis_title'])
+        axis.set_xlabel('Record index')
+        axis.set_ylim(bottom=settings['y-axis_min'], top=settings['y-axis_max'])
+
         for vib in range(1, fleets + 1):
-            vib_axis = []
-            vib_data = []
-            for record in cls.vib_attributes[]
-                vib_axis.append()
+            vib_axis = cls.vaps_records_df[
+                cls.vaps_records_df['vibrator']==vib].index.to_list()
+            vib_data = cls.vaps_records_df[
+                cls.vaps_records_df['vibrator']==vib][key].to_list()
+
+            if vib_data:
+                vib_data = np.array(vib_data)
+                axis.plot(vib_axis, vib_data, label=vib)
+
+        # axis.legend(loc='upper right')
+
+        return axis
+
+    @classmethod
+    def plot_density(cls, axis, settings):
+        '''  method to plot the attribute density function. If no density plot can be
+             made then plot unity density
+        '''
+        def dirac_function(x):
+            if x == 0:
+                return 1
+            else:
+                return 0
+
+        x_values = np.arange(
+            settings['y-axis_min'],
+            settings['y-axis_max'],
+            settings['y-axis_int']
+        )
+
+        axis.set_title(settings['title'])
+        axis.set_ylabel(settings['y-axis_title'])
+
+        for vib in range(1, fleets + 1):
+            vib_data = cls.vaps_records_df[
+                cls.vaps_records_df['vibrator']==vib][settings['key']].to_list()
+
+            if not vib_data:
+                continue
+
+            try:
+                vib_density_data = stats.kde.gaussian_kde(vib_data)
+                axis.plot(x_values, vib_density_data(x_values), label=vib)
+
+            except (np.linalg.LinAlgError):
+                vib_data = [dirac_function(x) for x in range(len(x_values))]
+                axis.plot(x_values, vib_data, label=vib)
+
+            axis.legend(loc='upper right')
+
+        return axis
 
 
 if __name__ == "__main__":
     vp_attr = VpAttributes()
 
-    vp_attr.select_date(prod_date)
-
+    production_date = datetime.datetime(2019, 12, 23)
+    vp_attr.select_data(production_date)
+    vp_attr.plot_vaps_data()
