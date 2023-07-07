84521efa1057c7e639613f86200ce6ede24a6dee
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2023-07-07 10:29:15 +0400

    vp_check_design refactored

--- vp_check_design.py
+++ vp_check_design.py
@@ -1,68 +1,133 @@
-''' this module checks the vps that are sent to processing and stored in the database
-    with the sps design vps
-    input: sps source design
-    output: sps source with a column add with count how many times the vp has been
-        acquired
+"""
+    This module checks recorded VPs stored in the SPS table in the database
+    with designed VPs. Repeated VPs are counted and a design_flag is set
+    if a VP was in the original design. The output is a CSV file.
+    arguments:
+        Two arguments must be given to the program:
+        - file name containing the design VPs and must have at least the
+          following fields (separated by one or more spaces):
+            column1, line, point, index
+            (only line, point and index are used as columns 2, 3 and 4)
+        - name of the sps table in the database
+    output:
+        - CSV file with recorded VPs with the following fields (separated
+          by comma):
+            line, point, easting, northing, elevation, count, design_flag
+        - count is the number of times a VP has been repeated
+        - design_flag is True if a VP was in the design, False if not
     pre-condition:
         - check is done on a block by block basis
-        - vps sent to processing must be stored in the database
-'''
+        - recorded vps must be stored in the database in the sps table
+
+    © 2023 Howdimain
+    bruno.vermeulen@hotmail.com
+"""
 import sys
+import pandas as pd
 import numpy as np
-import csv
 from pathlib import Path
-import seis_utils
 from seis_sps_database import SpsDb
 
 
-class Src:
+def write_message(message_id, message_var=None):
+    message_var = message_var if message_var else ""
+    match message_id:
+        case "db_sps":
+            message_text = "Getting the recorded VP records for "
+        case "source_design":
+            message_text = "Getting the SPS source design records from file: "
+        case "crosscheck":
+            message_text = "Crosscheck acquired VPs with design"
+        case "csv_out":
+            message_text = f"Writing VP count CSV file: "
+        case "done":
+            print("Done.")
+            return
+        case "error":
+            print(f"Error: {message_var}")
+            return
+        case other:
+            print(f"Invalid message: {message_var}.")
+            return
 
-    @classmethod
-    def check_design_sps(cls, sps_design_file, block_name):
-        linepoints = SpsDb().get_all_line_points(block_name)
-
-        progress_message = seis_utils.progress_message_generator(
-            f'check design sps for {sps_design_file.name}                 ')
-
-        with open(sps_design_file, mode='rt') as sps_design:
-            sps_lines = sps_design.readlines()
-
-        csv_output_file = sps_design_file.with_suffix('.csv')
-        with open(csv_output_file, 'w', newline='') as csvfile:
-            csv_write = csv.writer(csvfile, delimiter=',')
-            for sps_line in sps_lines:
-                sps_line = sps_line.replace('\n', '')
-                line, point = cls.parse_line(sps_line)
-                if line == -1 or point == -1:
-                    continue
-
-                linepoint = line * 10_000 + point
-                count = np.count_nonzero(linepoints == linepoint)
-                csv_elements = sps_line.split()
-                csv_elements.append(count)
-                csv_write.writerow(csv_elements)
-
-                next(progress_message)
-
-    @staticmethod
-    def parse_line(row):
-        try:
-            line = int(float(row[5:11]))
-            point = int(float(row[15:21]))
-            return line, point
-
-        except ValueError:
-            return -1, -1
+    print("".join([message_text, f"{message_var} ..."]))
 
 
-def main(sps_design_file_raw, block_name):
-    sps_design_file = Path(sps_design_file_raw)
-    Src().check_design_sps(sps_design_file, block_name)
+def check_design_sps(sps_design_file, block_name):
+    """
+    Function to check the final SPS source records with the
+    designed VPs. A count is made for repeated VPs. Compensation
+    for skips that are in the final SPS but obviously not the
+    design are added to the output CSV file
+    """
+    # create the output CSV file
+    sps_extended_file = sps_design_file.parent / Path(
+        "".join([sps_design_file.stem, "_count", ".csv"])
+    )
+
+    # get the sps records from the database
+    write_message("db_sps", f"'{block_name}'")
+    sps_df = SpsDb().get_all_line_points(block_name)
+    if sps_df.empty:
+        write_message(
+            "error", f"SPS table for '{block_name}' is empty, load these first"
+        )
+        exit()
+
+    sps_df["count"] = pd.Series(np.ones(sps_df.size))
+    sps_df = sps_df.groupby(["line", "point"], as_index=False).aggregate(
+        {
+            "line": "last",
+            "point": "last",
+            "easting": "mean",
+            "northing": "mean",
+            "elevation": "mean",
+            "count": "sum",
+        }
+    )
+    sps_df[["line", "point", "count"]] = sps_df[["line", "point", "count"]].astype(int)
+
+    # get the source design records from file
+    write_message("source_design", f"'{sps_design_file}'")
+    try:
+        sps_design_df = pd.read_csv(
+            sps_design_file, delimiter=r"\s+", usecols=[1, 2, 3]
+        )
+        if sps_design_df.empty:
+            write_message("error", f"unable to read '{sps_design_file}'")
+            exit()
+    except Exception as e:
+        write_message("error", f"'{sps_design_file}': {e}")
+        exit()
+
+    sps_design_df.columns = ["line", "point", "design"]
+    sps_design_df[["line", "point", "design"]] = sps_design_df[
+        ["line", "point", "design"]
+    ].astype(int)
+
+    # doing a union between recorded VPs in SPS and the designed VPs
+    write_message("crosscheck", None)
+    sps_df = pd.merge(sps_df, sps_design_df, on=["line", "point"], how="outer").fillna(
+        0
+    )
+    sps_df[["line", "point", "count"]] = sps_df[["line", "point", "count"]].astype(int)
+    sps_df[["design"]] = sps_df[["design"]].astype(bool)
+
+    # write result as csv file
+    write_message("csv_out", f"'{sps_extended_file}'")
+    sps_df.to_csv(sps_extended_file, index=False)
+
+    write_message("done", None)
 
 
-if __name__ == '__main__':
+def main(sps_design_file, block_name):
+    sps_design_file = Path(sps_design_file)
+    check_design_sps(sps_design_file, block_name)
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print('give the design SPS source file and block name')
+        write_message("error", "Give the design SPS source file and block name")
         exit()
 
     main(sys.argv[1], sys.argv[2])
