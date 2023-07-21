60c94d6c7926430aa0591f09da4f7eaf0e1c3465
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2023-07-21 12:10:19 +0400

    black seis_vibe_database, extended_qc get location

--- vp_extended_qc.py
+++ vp_extended_qc.py
@@ -1,38 +1,91 @@
 """ application to work with vibrator extended QC
 """
 from pathlib import Path
+import datetime
+import pandas as pd
 import vp_extended_qc_parser as parser
-from pprint import pprint
+from seis_vibe_database import VpDb
+from seis_settings import GMT_OFFSET
 
 
 class VpExtendedQc:
     def __init__(self, filename):
         self.filename = filename
+        self.vaps_df = None
 
-    def attributes_avg_peak(self):
+    def get_location(self, production_date, vibrator_id, time_break):
+        if self.vaps_df is None:
+            self.vaps_df = VpDb().get_vp_data_by_date("VAPS", production_date)
+            self.vaps_df["time_break"] = pd.to_datetime(
+                self.vaps_df["time_break"], format="ISO8601"
+            )
+        try:
+            s_line, s_point = self.vaps_df[
+                (self.vaps_df["time_break"] == time_break)
+                & (self.vaps_df["vibrator"] == vibrator_id)
+            ][["line", "point"]].values[0]
+        except IndexError:
+            s_line, s_point = -1, -1
+
+        return s_line, s_point
+
+    def vp_attributes(self, location=False):
         extended_qc_iterator = parser.extended_qc_generator(self.filename)
+        vaps_df = None
+        columns_attributes_df = [
+            "line",
+            "station",
+            "vibrator",
+            "avg_phase",
+            "peak_phase",
+            "avg_dist",
+            "peak_dist",
+            "avg_force",
+            "peak_force",
+            "avg_target_foce",
+            "start_time",
+            "time_break",
+        ]
+        attributes_df = pd.DataFrame(columns=columns_attributes_df)
+        self.vaps_df = None
         for extended_qc_record in extended_qc_iterator:
-            df = extended_qc_record.attributes_df
-            avg_vals = df[2:].mean()
-            peak_vals = df[2:].max()
-            attributes_dict = {
-                "vib": extended_qc_record.vibrator_id,
-                "line": extended_qc_record.source_line,
-                "station": extended_qc_record.station_number,
-                "time_break": extended_qc_record.time_break,
-                "start_time": df.iloc[2]["time"],
-                "avg_phase": round(avg_vals["phase"]),
-                "peak_phase": round(peak_vals["phase"]),
-                "avg_dist": round(avg_vals["dist"]),
-                "peak_dist": round(peak_vals["dist"]),
-                "avg_force": round(avg_vals["force"]),
-                "peak_force": round(peak_vals["force"]),
-                "avg_target_force": round(avg_vals["target"]),
-            }
-            pprint(attributes_dict)
-            input()
+            production_date = (extended_qc_record.time_break + GMT_OFFSET).date()
+            ext_qc_df = extended_qc_record.attributes_df
+            avg_vals = ext_qc_df[2:].mean()
+            peak_vals = ext_qc_df[2:].max()
+            tb_ext_qc = extended_qc_record.time_break
+            tb_vaps = tb_ext_qc + GMT_OFFSET
+            vibrator_id = extended_qc_record.vibrator_id
+            s_line, s_point = (
+                self.get_location(production_date, vibrator_id, tb_vaps)
+                if location
+                else (-1, -1)
+            )
+            attributes_list = [
+                s_line,
+                s_point,
+                vibrator_id,
+                round(avg_vals["phase"]),
+                round(peak_vals["phase"]),
+                round(avg_vals["dist"]),
+                round(peak_vals["dist"]),
+                round(avg_vals["force"]),
+                round(peak_vals["force"]),
+                round(avg_vals["target"]),
+                ext_qc_df.iloc[2]["time"],
+                tb_ext_qc,
+            ]
+            attributes_df = pd.concat(
+                [
+                    attributes_df,
+                    pd.DataFrame([attributes_list], columns=columns_attributes_df),
+                ],
+                ignore_index=True,
+            )
+
+        print(attributes_df)
 
 
 if __name__ == "__main__":
     extended_qc = VpExtendedQc(Path("./data_files/230629_VIB08_test.txt"))
-    extended_qc.attributes_avg_peak()
+    extended_qc.vp_attributes(location=True)
